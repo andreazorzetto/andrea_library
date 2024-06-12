49cc29fc9b91d331a7f97beabe5444b0a4294560
Andrea Zorzetto <andrea@thatpersonalmail.com>
2024-06-12 16:27:36 +0100

    return json instead of custom

--- examples/saas_get_token.py
+++ examples/saas_get_token.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import json
 from andrea_library.aqua_functions import get_token
 
 # Load environment variables from .env file
@@ -12,11 +13,12 @@ api_secret = os.getenv('API_SECRET')
 api_methods = os.getenv('API_METHODS')
 aqua_role = os.getenv('AQUA_ROLE')
 
+# Authentication
 # Call the get_token function
 try:
     token = get_token(base_url, api_key, api_secret, aqua_role, api_methods)
-    print(f'Token: {token}')
+    print(json.dumps({"Token": token}))
 
 except Exception as e:
     # Handle the error as needed (e.g., logging, displaying an error message to the user)
-    print(f'Error occurred: {e}')
+    print(json.dumps({"Error occurred": e}))
